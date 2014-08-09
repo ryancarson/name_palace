b57f10c2d344f1b7a8186b000ed2a54109508a64
Ryan Carson <ryan@teamtreehouse.com>
2014-08-08 19:18:43 -0700

    Added some comments

--- name_palace.py
+++ name_palace.py
@@ -1,9 +1,12 @@
 from collections import namedtuple
 
+# A palace is a way to remember things. You create 'rooms' and then place things you want to remember in each room. Then when you need to recall, you imagine yourself walking through the rooms in your 'palace'
+# To read more about this method, head to http://en.wikipedia.org/wiki/Method_of_loci
 palace = []
 
 memory = namedtuple('memory', ['room', 'name'])
 
+# Insert some hard-coded data to make sure it works
 palace.append(memory(room='Kitchen', name='Ryan'))
 palace.append(memory(room='Dining Room', name='Gill'))
 
