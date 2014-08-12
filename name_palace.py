e52f5f32ab37b5f18695ea1cb18d7a030578495d
Ryan Carson <ryan@teamtreehouse.com>
2014-08-11 17:46:17 -0700

    Changed indenting to 4 spaces, Used camel-case for Named Tuple, Cleaned up input text prompts and wrapped the whole thing in a function to avoid problems with imports in the future

--- name_palace.py
+++ name_palace.py
@@ -1,48 +1,55 @@
-from collections import namedtuple
+def memory_palace():
 
-'''
-A palace is a way to remember things. You create 'rooms' and then place things
-you want to remember in each room. Then when you need to recall, you imagine
-yourself walking through the rooms in your 'palace'
-To read more about this method, head to
-http://en.wikipedia.org/wiki/Method_of_loci
-'''
-palace = []
-rooms = []
+    from collections import namedtuple
 
-'''
-The idea is to place a person in a room, doing a memorable action.
-The weirder and more memorable the action, the better. For instance
-Ryan in the kitchen, roasting a basketball on the stove - "Ryan" and
-"roasting" being memorable
-'''
+    '''
+    A palace is a way to remember things. You create 'rooms' and then place things
+    you want to remember in each room. Then when you need to recall, you imagine
+    yourself walking through the rooms in your 'palace'
+    To read more about this method, head to
+    http://en.wikipedia.org/wiki/Method_of_loci
+    '''
+    palace = []
+    rooms = []
 
-# TODO Catch errors
-# TODO Test on name memory at certain intervals (review)
-# TODO Add tags to people like school, work, etc
+    '''
+    The idea is to place a person in a room, doing a memorable action.
+    The weirder and more memorable the action, the better. For instance
+    Ryan in the kitchen, roasting a basketball on the stove - "Ryan" and
+    "roasting" being memorable
+    '''
 
-memory = namedtuple('memory', ['room', 'name', 'action'])
+    # TODO Catch errors
+    # TODO Test on name memory at certain intervals (review)
+    # TODO Add tags to people like school, work, etc
+    # TODO Add database to store values
+    # TODO Convert to OOP
 
-print("Let's create your name palace first. Imagine yourself walking through your house, and enter the names of the rooms in that order. Please enter the room's name (for example Dining Room). Enter DONE when you're finished")
+    Memory = namedtuple('Memory', ['room', 'name', 'action'])
 
-while True:
-  room = input("> ")
-  if room.lower() == "done":
-    break
-  rooms.append(room)
+    print("Let's create your name palace first.\n"
+          "Imagine yourself walking through your house, and enter the names \n"
+          "of the rooms in that order. Enter DONE when you're finished")
 
-print("OK. Here are the rooms in your Memory Palace:")
-for s in rooms:
-  print(s)
+    while True:
+        room = input("> ")
+        if room.lower() == "done":
+          break
+        rooms.append(room)
 
-print("Now you can start putting names and actions into those rooms. The more crazy and memorable the actions, the better!")
+    print("\nNow you can start putting names and actions into those rooms.\n"
+          "The more crazy and memorable the actions, the better!")
 
-for s in rooms:
-  name = input("\nRoom: "+s+"\nName: ")
-  action = input("Action: ")
-  palace.append(memory(room=s, name=name, action=action))
+    for room in rooms:
+        print("\nRoom: "+room)
+        name = input("Name: ")
+        action = input("Action: ")
+        palace.append(Memory(room=room, name=name, action=action))
 
-print("OK, great! Here is your completed Memory Palace:\n")
+    print("\nOK, great! Here is your completed Memory Palace ...\n")
 
-for memory in palace:
-  print("Room: " + memory.room + ", Name: " + memory.name + ", Action: " + memory.action)
+    for Memory in palace:
+        print("Room: " + Memory.room + ", Name: " + Memory.name + ", Action: " + Memory.action)
+
+if __name__ == '__main__':
+    memory_palace()
