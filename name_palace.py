51282838cd39ac74ddb637f55f51f55de0c9e1eb
Chris Upjohn <ChrisUpjohn@users.noreply.github.com>
2014-10-13 17:37:11 +1100

    Update name_palace.py

--- name_palace.py
+++ name_palace.py
@@ -5,7 +5,6 @@ class Name_Palace:
   rooms = []
   
   def add_memories(self):
-    
     print("\nNow you can start putting names and actions into those rooms.\n"
           "The more crazy and memorable the actions, the better!!\n")
     
@@ -15,20 +14,19 @@ class Name_Palace:
       action = input('Action: ')
       print('\n')
       room.add_memory(person, action)
-        
   
   def __init__(self):
     while True:
       choice = input("Type your room name or 'DONE' to finish: ")
       if choice.lower() == 'done':
         break
-      else:
-        self.rooms.append(Room(name = choice))
+      
+      self.rooms.append(Room(name = choice))
     
     self.add_memories()
     
     print("\nOK, great! Here is your completed Memory Palace ...\n")
     for room in self.rooms:
-        print("Room: {}, Person's Name: {}, Action: {}".format(room.name, room.person_name, room.action))
-        
+      print("Room: {}, Person's Name: {}, Action: {}".format(room.name, room.person_name, room.action))
+    
 Name_Palace()
